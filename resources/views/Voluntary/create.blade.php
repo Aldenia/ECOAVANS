7f0b1f8d593d5acbbba14e579d33fb93bf5bdb0e
Aldenia <aldenia.briones.cortes@est.una.ac.cr>
2020-09-03 14:52:13 -0600

    Create

---
+++ resources/views/Voluntary/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+@section('content')
+ <div class="col-sm-8">
+ <h2>
+ Nuevo cliente
+ <a href="{{ route('Voluntary.index')}}" class="btn btn-default
+pull-right">regresar</a>
+ </h2>
+ @include('clientes.fragments.errors')
+ {!! Form::open(['route'=>'Voluntary.store']) !!}
+ @include('Voluntary.fragments.form')
+ {!! Form::close() !!}
+ </div>
+
+@endsection
