f90e9309789ca5cf75754bf413299ea266809d16
Carlos Antonio <carlos.rodriguez.herrera@est.una.ac.cr>
2020-09-03 13:53:54 -0600

    Voluntary MVC

---
+++ app/Voluntary.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Voluntary extends Model
+{
+    protected $fillable =[
+        'Id',
+        'Nombre',
+        'Apellido.1', 
+        'Apellido.2',
+        'Telefono',
+        'Direccion',
+        'Email',
+        'Descripcion'
+    ];
+    
+}
