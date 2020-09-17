5c27758ae6a0cf59d4d461af482de79423cfd0ff
Aldenia <aldenia.briones.cortes@est.una.ac.cr>
2020-09-16 22:30:19 -0600

    Cambios de información y color

--- app/Information.php
+++ app/Information.php
@@ -7,26 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Information extends Model
 {
     protected $fillable = [
-<<<<<<< HEAD
-<<<<<<< HEAD
-        // jorge´s test
-        //Jorge tiene hambre <3
 
-        'name'
-
-=======
-        
-        'name_U'
-//hhhhhhh
->>>>>>> ccf2b62baa278f24811a580f38e33068ea477bc2
-=======
-        'name',
-        'lastname',
-        'id',
-
-        
-        'name_U'
-
->>>>>>> bedd2ad89412c830f95360bf47f7e23d212d04f2
      ];
 }
