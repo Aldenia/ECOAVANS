d699ffd39b99089846745508a1ac5cac8a1c7399
Carlos Antonio <carlos.rodriguez.herrera@est.una.ac.cr>
2020-10-15 21:05:59 -0600

    Donaciones

--- app/Donation.php
+++ app/Donation.php
@@ -25,10 +25,3 @@ class Donation extends Model
 
     protected $table = 'donation';
 }
-    protected $fillable = ['id', 'donorName', 'donationType', 'incomes_id'];
-
-    public function Income()
-    {
-     return $this->belongsTo('App\Income');
-    }
-}
